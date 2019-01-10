b13cbeac014f1d3f11de37b93c39a105040eb557
flying <875512109@qq.com>
2019-01-10 11:27:30 +0800

    compact

--- lodash/syf875512109-lodash.js
+++ lodash/syf875512109-lodash.js
@@ -1,8 +1,20 @@
 var syf875512109 = (function(){
 
+  function compact(array, size=1) {
+    let arr = [];
+    let temp;
+    let j = 0;
+    for(let i = 0; i < array.length; i++) {
+      temp = array[i];
+      if (!(temp == false && (!temp != false)) || !isNaN(temp)) {
+        arr[j++] = temp;
+      }
+    }
+    return arr;
+  }
 
 
   return {
-    
+    compact: compact,
   }
 })()
